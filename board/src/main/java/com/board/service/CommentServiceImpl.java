2cb4ffc4289764d410c2577bfdaa48387454906a
hwarak <hwarakhada@gmail.com>
2021-05-04 23:18:01 +0900

    재업로드

--- board/src/main/java/com/board/service/CommentServiceImpl.java
+++
@@ -1,5 +0,0 @@
-package com.board.service;
-
-public class CommentServiceImpl implements CommentService{
-
-}
--- board/src/main/java/com/grabme/vo/CategoryVO.java
+++
@@ -1,9 +0,0 @@
-package com.grabme.vo;
-
-import lombok.Data;
-
-@Data
-public class CategoryVO {
-	private int categoryIdx;
-	private String categoryName;
-}
